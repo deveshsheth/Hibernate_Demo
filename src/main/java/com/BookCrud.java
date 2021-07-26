e511d9672e7df0d3c74855bfefd168d06d2f452a
deveshsheth <deveshssheth2018@gmail.com>
2021-07-26 11:18:54 +0530

    first commit

---
+++ src/main/java/com/BookCrud.java
@@ -0,0 +1,93 @@
+package com;
+
+import java.util.List;
+import java.util.Scanner;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+import com.bean.BookBean;
+
+public class BookCrud {
+	
+	public static void main(String[] args) {
+		int ch = -1;
+		SessionFactory sf = new Configuration().configure().buildSessionFactory();
+		Session session = null;
+		Scanner sc = new Scanner(System.in);
+		String name,author;
+		int bookId,price,quantity;
+		Transaction tx = null;
+		while(true) {
+			System.out.println(
+					"\n0 for Exit\n1 For AddBook\n2 For List All Books\n3 For Delete Book By Id\n4 For Get Book Detail By Id");
+			System.out.println("\n5 For Update book\n6 for SearchBook By Name\nEnter Choice");
+			ch = sc.nextInt();
+			
+			switch(ch) {
+			case 0:
+				System.exit(0);
+				break;
+			
+			case 1:
+				System.out.println("Enter Book Name : ");
+				name = sc.next();
+				System.out.println("Enter Book Author : ");
+				author = sc.next();
+				System.out.println("Enter Book Price : ");
+				price = sc.nextInt();
+				System.out.println("Enter Book Quantity : ");
+				quantity = sc.nextInt();
+				
+				BookBean book = new BookBean();
+				book.setName(name);
+				book.setAuthor(author);
+				book.setPrice(price);
+				book.setQuantity(quantity);
+				
+				session = sf.openSession();
+				tx = session.beginTransaction();
+				try {
+					session.save(book);
+					tx.commit();
+				}catch (Exception e) {
+					// TODO: handle exception
+					tx.rollback();
+				}finally {
+					session.close();
+				}
+				break;
+				
+			case 2:
+				session = sf.openSession();
+				List<BookBean> books = session.createQuery("from BookBean", BookBean.class).getResultList();
+
+				for (BookBean bean : books) {
+					System.out.println(bean.getBookId() + " " + bean.getName() + " " + bean.getAuthor() + " " + bean.getPrice()+" "+bean.getQuantity());
+				}
+
+				session.close();
+				break;
+				
+			case 4:
+				System.out.println("Enter BookId :  ");
+				bookId = sc.nextInt();
+				session = sf.openSession();
+				book = session.get(BookBean.class, bookId);
+				System.out.println(
+						book.getBookId() + " " + book.getName() + " " + book.getAuthor() + " " + book.getPrice()+" "+book.getQuantity());
+				break;
+				
+			default:
+				System.out.println("\nInvalid choice try again!!!");
+				break;
+			
+			}
+		}
+		
+		
+	}
+
+}
---
+++ src/main/java/com/bean/BookBean.java
@@ -0,0 +1,43 @@
+package com.bean;
+
+public class BookBean {
+	
+	int bookId,quantity,price;
+	String name;
+	String author;
+	public int getBookId() {
+		return bookId;
+	}
+	public void setBookId(int bookId) {
+		this.bookId = bookId;
+	}
+	public int getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	
+	
+	
+	
+
+}
