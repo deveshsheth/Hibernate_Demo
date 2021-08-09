9c6d0108a7cd929bb11c576a62d4c00b9cf8efb8
deveshsheth <deveshssheth2018@gmail.com>
2021-08-09 21:10:53 +0530

    crud

--- src/main/java/com/StudentCourse.java
+++ src/main/java/com/StudentCourse.java
@@ -15,6 +15,30 @@ import com.bean.StudentBean;
 public class StudentCourse {
 	
 	public static void main(String[] args) {
+		
+		//new StudentCourse().addStudent();
+		// new StudentCourse().getStudent();
+		// new StudentCourse().deleteStudent();
+		// new StudentCourse().addCertiForStudent();
+		new StudentCourse().deleteCertificate();
+		
+	}
+	
+	void getStudent() {
+
+		SessionFactory sf = new Configuration().configure().buildSessionFactory();
+		Session session = sf.openSession();
+
+		StudentBean sb = session.get(StudentBean.class, 11);// student
+		System.out.println(sb.getFirstName());
+
+		for (CertificateBean cb : sb.getCertificatBean()) {
+			System.out.println(cb.getName());
+		}
+		session.close();
+	}
+	
+	void addStudent() {
 		SessionFactory sf = new Configuration().configure().buildSessionFactory();
 		Session session = sf.openSession();
 		
@@ -31,7 +55,12 @@ public class StudentCourse {
 		
 		StudentBean sb1 = new StudentBean();
 		sb1.setFirstName("Devesh");
-		sb1.setFirstName("Abhishek");
+		
+		StudentBean sb2 = new StudentBean();
+		sb2.setFirstName("Aryan");
+		
+		StudentBean sb3 = new StudentBean();
+		sb3.setFirstName("Abhishek");
 		
 		ArrayList<CertificateBean> certificateBeans = new ArrayList<CertificateBean>();
 		
@@ -40,11 +69,62 @@ public class StudentCourse {
 		certificateBeans.add(c3);
 		
 		sb1.setCertificatBean(certificateBeans);
+		sb2.setCertificatBean(certificateBeans);
+		sb3.setCertificatBean(certificateBeans);
 		
-		session.save(sb1); 
+		session.save(sb1);
+		session.save(sb2);
+		session.save(sb3);
 		tx.commit();
 		session.close();
 	}
+	
+	void deleteStudent() {
+		SessionFactory sf = new Configuration().configure().buildSessionFactory();
+		Session session = sf.openSession();
+		Transaction tx = session.beginTransaction();
+
+		StudentBean studentBean = session.get(StudentBean.class, 3);
+		session.delete(studentBean);
+
+		tx.commit(); 
+		session.close();
+
+	}
+	
+	void addCertiForStudent() {
+
+		SessionFactory sf = new Configuration().configure().buildSessionFactory();
+		Session session = sf.openSession();
+
+		Transaction tx = session.beginTransaction();
+
+		CertificateBean cb = new CertificateBean();
+		cb.setName("RoR");
+
+		StudentBean sb = session.get(StudentBean.class, 4);
+		sb.getCertificatBean().add(cb);
+
+		session.save(sb);
+
+		tx.commit();
+		session.close();
+
+	}
+	
+	void deleteCertificate() {
+		SessionFactory sf = new Configuration().configure().buildSessionFactory();
+		Session session = sf.openSession();
+
+		Transaction tx = session.beginTransaction();
+
+		CertificateBean cb =  session.get(CertificateBean.class, 13);
+		session.delete(cb);
+
+		tx.commit();
+		session.close();
+	}
+
 	
 	
 }
